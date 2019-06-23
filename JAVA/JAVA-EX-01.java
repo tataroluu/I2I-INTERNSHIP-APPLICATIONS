d01de8acd562edf2f874a55d20e4fa52e37bf715
tataroluu <49092564+tataroluu@users.noreply.github.com>
2019-06-24 00:26:06 +0300

    Create JAVA-EX-01.java

---
+++ JAVA/JAVA-EX-01.java
@@ -0,0 +1,23 @@
+for(int i=0;i<20;i++)
+		{
+			if(i==1) 
+			{
+						
+			}
+			else if(i%2!=0 && i%3!=0 ) 
+			{	
+				int number=0;	
+				int	total=number+1;
+					total=number+1;
+						System.out.println(i);
+			}
+					
+			else if(i==2)
+			{
+				System.out.println(i);
+			}
+			else if(i==3)
+			{
+				System.out.println(i);
+			}
+	 }
