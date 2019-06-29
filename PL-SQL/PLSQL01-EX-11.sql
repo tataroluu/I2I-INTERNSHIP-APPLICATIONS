3a0bd56d5487a8deddb642e3aeb00285f938ae5a
tataroluu <49092564+tataroluu@users.noreply.github.com>
2019-06-29 14:09:48 +0300

    Update PLSQL01-EX-11.sql

--- PL-SQL/PLSQL01-EX-11.sql
+++ PL-SQL/PLSQL01-EX-11.sql
@@ -1,1 +1,31 @@
+SET SERVEROUTPUT ON;
 
+DECLARE 
+
+    i number;
+    k number;
+    j number;
+
+BEGIN
+
+FOR i IN  0..7
+LOOP
+
+FOR j IN i..7
+LOOP
+
+    dbms_output.put(' '); 
+
+END LOOP;
+
+FOR k IN 0..(i*2)-2
+LOOP  
+
+                dbms_output.put('*'); 
+
+END LOOP; 
+
+            dbms_output.put_line(''); 
+            
+END LOOP;
+END;
