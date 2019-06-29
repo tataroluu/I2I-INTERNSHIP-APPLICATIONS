42cdac81c6626d421eb6113e41aa82b2f144b64e
tataroluu <49092564+tataroluu@users.noreply.github.com>
2019-06-29 16:31:49 +0300

    Update SQL01-EX-03.sql

--- PL-SQL/SQL01-EX-03.sql
+++ PL-SQL/SQL01-EX-03.sql
@@ -1,22 +1,64 @@
-SELECT SUBSTR( (PHONE_NUMBER), 1 , 3  ) 
-    AS Operator, 
-        COUNT(EMPLOYEE_ID) 
-            AS Total 
-                FROM HR.EMPLOYEES
-                    GROUP BY SUBSTR( (PHONE_NUMBER), 1 , 3  );
-
+SELECT 
+    SUBSTR
+    ( 
+    (PHONE_NUMBER),
+    1 ,
+    3  
+    ) 
+AS 
+    Operator, 
+COUNT
+    (EMPLOYEE_ID) 
+AS 
+    Total 
+FROM 
+    HR.EMPLOYEES
+GROUP BY 
+    SUBSTR( 
+    (PHONE_NUMBER), 
+    1 , 
+    3  
+    );
 ------------------------------------------------------------------------------
-SELECT * FROM (
-SELECT SUBSTR( (PHONE_NUMBER), 1 , 3  ) 
-    AS vi_operator, 
-        COUNT(EMPLOYEE_ID) 
-            AS vi_total 
-                FROM HR.EMPLOYEES
-                    GROUP BY SUBSTR( (PHONE_NUMBER), 1 , 3  )
-)
+SELECT 
+    * 
+FROM 
+(
+SELECT 
+    SUBSTR
+    ( 
+    (PHONE_NUMBER), 
+    1 , 
+    3  
+    ) 
+AS 
+    vi_operator, 
+COUNT
+    (EMPLOYEE_ID) 
+AS 
+    vi_total 
+FROM 
+    HR.EMPLOYEES
+GROUP BY 
+    SUBSTR
+    ( 
+    (PHONE_NUMBER), 
+    1 , 
+    3  
+    )
+    )
 PIVOT
 (
-    SUM (vi_total) 
-        FOR vi_operator 
-            IN(515,590,603,011,650)
+SUM 
+    (vi_total) 
+FOR 
+    vi_operator 
+IN
+(
+    515,
+    590,
+    603,
+    011,
+    650
+)
 );
