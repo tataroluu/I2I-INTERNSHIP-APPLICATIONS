45427effea2b2138205cd62642eddfc10ceaf4e1
tataroluu <49092564+tataroluu@users.noreply.github.com>
2019-06-29 16:07:36 +0300

    Update PLSQL01-EX-01.sql

--- PL-SQL/PLSQL01-EX-01.sql
+++ PL-SQL/PLSQL01-EX-01.sql
@@ -1,13 +1,11 @@
 SET SERVEROUTPUT ON;
 DECLARE
 BEGIN
-FOR
-  i IN 0..20
-        LOOP
-             IF ( MOD (i,2)=0 ) THEN
-            
- 	       dbms_output.put_line(i);  
-            
-	     END IF;
-        END LOOP;
+FOR i IN 0..20
+LOOP
+IF ( MOD (i,2)=0 ) 
+THEN
+    dbms_output.put_line(i);  
+END IF;
+END LOOP;
 END;
